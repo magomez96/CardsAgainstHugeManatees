00883a56ee60f94ced65074079859ef6e771232a
Matthew Gomez <magomez96@gmail.com>
2016-04-11 22:01:19 -0400

    Implemented the ability to limit the number of players in a game.

--- src/botCommands.py
+++ src/botCommands.py
@@ -13,7 +13,7 @@ def process(bot, chat_id, parsedCommand, messageText, currentMessage, update, in
         botSendFunctions.sendText(bot, chat_id, givenText, replyingMessageID, keyboardLayout)
 
     gameRecords = Base("chatStorage/records.pdl") # The following two lines create a DB to map gameID's to group chat ID's and player ID's
-    gameRecords.create("gameID", "groupChatID", "memberUsernames", "memberUserIDs", "memberChatIDs", "memberPoints", "creator", "creatorChatID", "started", mode="open")
+    gameRecords.create("gameID", "groupChatID", "memberUsernames", "memberUserIDs", "memberChatIDs", "memberPoints", "creator", "creatorChatID", "playerLimit", "started", mode="open")
     gameRecords.create_index("gameID") # Create a index to make selections by gameID
     gameRecords.create_index("groupChatID") # Create a index to make selections by groupChatID
     gameRecords.commit() # Save changes to disk
@@ -26,7 +26,7 @@ def process(bot, chat_id, parsedCommand, messageText, currentMessage, update, in
         print("Processing command " + messageText)
         if parsedCommand == "/help" or parsedCommand == "/start": # The default command Telegram sends to a bot is /start
             s = "This is the Cards Against Huge Manatees Bot\n"
-            s += "To create a game type /newgame in a group chat\n"
+            s += "To create a game type /newgame in a group chat or /newgame number to limit the number of players\n"
             s += "To join a game type /join [gameID] in a private chat with the bot\n"
             s += "To start the game after everyone has joined type /startgame in the group chat.\n"
             s += "A new black card will appear in the group chat and your white cards will appear\n"
@@ -38,15 +38,25 @@ def process(bot, chat_id, parsedCommand, messageText, currentMessage, update, in
             sendText(s)
         elif parsedCommand == "/newgame":
             ident = game.generate() # Generate a game ID
-            gameRecords.insert(ident, str(chat_id), "", "", "", "", currentMessage.from_user.id, "", 0) # Make a new database record skeleton
+            if not len(currentMessage) > 9:
+                gameRecords.insert(ident, str(chat_id), "", "", "", "", currentMessage.from_user.id, "", 0) # Make a new database record skeleton
+            else:
+                try:
+                    gameRecords.insert(ident, str(chat_id), "", "", "", "", currentMessage.from_user.id, "", abs(int(currentMessage.text.split()[1])), 0) # Make a new database record skeleton
+                except Exception:
+                    gameRecords.insert(ident, str(chat_id), "", "", "", "", currentMessage.from_user.id, "", 0) # Make a new database record skeleton
             gameRecords.commit()
             sendText("The game ID is " + ident + " Please type /join " + ident + " in a private chat with the bot.") # Send the ID to the group chat
+
         elif parsedCommand == "/join":
             rec = gameRecords._gameID[currentMessage.text.upper().split()[1]] # Get the DB record by GameID
             if not rec:
                 sendText("Game ID not found.")
                 return
             rec = rec[-1]
+            if(len(rec['memberUserIDs']) >= int(rec['playerLimit'])):
+                sendText("Sorry. The game is full.")
+                return
             if rec['started']: # If the game has already started they can't join.
                 sendText("The game has already started. Sorry.")
                 return
