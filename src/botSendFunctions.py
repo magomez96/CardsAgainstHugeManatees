6cde7974f5c5028120a3bb8cdf409f788051ed30
Matthew Gomez <magomez96@gmail.com>
2016-04-14 13:05:30 -0400

    Add returns to facilitate unit testing and change file open mode to read only.

--- src/botSendFunctions.py
+++ src/botSendFunctions.py
@@ -1,17 +1,18 @@
 # This file contains functions for interacting with the Telegram API using the library
+# The returns in this function are not necessary for the bot to function, but they were
+# added to facilitate testing using PyUnit
 import telegram
 
 def sendText(bot, chat_id, messageText, replyingMessageID=0, keyboardLayout=[], killkeyboard=True):
     bot.sendChatAction(chat_id=chat_id, action='typing')
-    try:
-        print("Sending " + messageText + " to " + str(chat_id))
-    except Exception as e:
-        print(str(e))
-
+    print("Sending " + messageText + " to " + str(chat_id))
     if replyingMessageID != 0:
         bot.sendMessage(chat_id=chat_id, text=messageText, reply_to_message_id=replyingMessageID, reply_markup=telegram.ReplyKeyboardHide(hide_keyboard=killkeyboard))
+        return messageText
     elif keyboardLayout != []:
         print("Sending custom keyboard")
         bot.sendMessage(chat_id=chat_id, text=messageText, reply_markup=telegram.ReplyKeyboardMarkup(keyboard=keyboardLayout, one_time_keyboard=True, resize_keyboard=True))
+        return keyboardLayout
     else:
         bot.sendMessage(chat_id=chat_id, text=messageText, reply_markup=telegram.ReplyKeyboardHide(hide_keyboard=killkeyboard))
+        return messageText
